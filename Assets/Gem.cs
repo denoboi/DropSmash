081357ae0c919c5c87a44df87a36ae0d7ed6253b
denoboi <deniz.kaymakci@hotmail.com>
2022-04-25 15:54:57 +0300

    Gem Moving Upper Right in UI
    If gems collide with the ground they move to the UI. Invoked in Gem.cs, listened in GemPanel.cs

--- Assets/Gem.cs
+++ Assets/Gem.cs
@@ -7,8 +7,19 @@ using UnityEngine;
 public class Gem : MonoBehaviour
 {
 
-   [Button]
-   void OnRevealed()
+    //when gems collide with the ground I moved them to the upper right corner. 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("HITTHEGROUND");
+            OnRevealed();
+            Destroy(gameObject);
+        }
+    }
+
+    //this is the method that throws gems to the upper right. I invoked it here and listened in GemPanel.cs
+    void OnRevealed()
     {
         EventManager.OnRevealed.Invoke(transform.position);
     }
