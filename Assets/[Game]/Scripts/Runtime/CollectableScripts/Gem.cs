3ff8e6f192f77906614102663edc6ca49ff970d8
denoboi <deniz.kaymakci@hotmail.com>
2022-04-29 20:22:15 +0300

    UI Fixes

--- Assets/[Game]/Scripts/Runtime/CollectableScripts/Gem.cs
+++ Assets/[Game]/Scripts/Runtime/CollectableScripts/Gem.cs
@@ -36,7 +36,7 @@ public class Gem : StatObjectBase
             Debug.Log("HITTHEGROUND");
             
             //Wait for ... sec after hit the ground
-            Invoke("OnRevealed", 3);
+            Invoke("OnRevealed", 2);
             
             
         }
